6237c77f3625e8d1aa372711153f30891e8ff869
HernanC95 <cavalieri.h95@gmail.com>
2024-10-16 11:34:49 -0300

    fawfef

--- src/App.tsx
+++ src/App.tsx
@@ -21,17 +21,14 @@ const App: React.FC = () => {
               title="Giphy 2"
             ></iframe>
             <iframe
-              src="https://giphy.com/embed/VvBwJPhs2QEbQ0UiCj"
-              frameBorder="0"
-            ></iframe>
-            <iframe
               src="https://giphy.com/embed/3o7btWxDlmXDVMdHLW"
               frameBorder="0"
             ></iframe>
           </div>
+
           {/* Texto superpuesto */}
           <div className={style.textHeader}>
-            <h1 className={style.title}>Titulfdwo</h1>
+            <h1 className={style.title}>TITULO</h1>
             <p className={style.description}>Descripcion de alguna boludez</p>
           </div>
 
--- src/Roulette.tsx
+++ src/Roulette.tsx
@@ -7,42 +7,62 @@ const data = [
   {
     option: "Prenda 1",
     style: { backgroundColor: "blue", textColor: "white" },
+    description:
+      "Descripción de la prenda 1: Camiseta de algodón cómoda y ligera.",
   },
   {
     option: "Prenda 2",
     style: { backgroundColor: "white", textColor: "blue" },
+    description:
+      "Descripción de la prenda 2: Sudadera con capucha ideal para el invierno.",
   },
   {
     option: "Prenda 3",
     style: { backgroundColor: "blue", textColor: "white" },
+    description:
+      "Descripción de la prenda 3: Pantalones vaqueros de corte ajustado.",
   },
   {
     option: "Prenda 4",
     style: { backgroundColor: "white", textColor: "blue" },
+    description:
+      "Descripción de la prenda 4: Chaqueta impermeable para días lluviosos.",
   },
   {
     option: "Prenda 5",
     style: { backgroundColor: "blue", textColor: "white" },
+    description:
+      "Descripción de la prenda 5: Camisa de vestir con diseño clásico.",
   },
   {
     option: "Prenda 6",
     style: { backgroundColor: "white", textColor: "blue" },
+    description:
+      "Descripción de la prenda 6: Pantalones cortos cómodos para el verano.",
   },
   {
     option: "Prenda 7",
     style: { backgroundColor: "blue", textColor: "white" },
+    description:
+      "Descripción de la prenda 7: Bufanda de lana para mantener el calor.",
   },
   {
     option: "Prenda 8",
     style: { backgroundColor: "white", textColor: "blue" },
+    description:
+      "Descripción de la prenda 8: Gorro de invierno hecho de tejido suave.",
   },
   {
     option: "Prenda 9",
     style: { backgroundColor: "blue", textColor: "white" },
+    description:
+      "Descripción de la prenda 9: Vestido de verano ligero y colorido.",
   },
   {
     option: "Prenda 10",
     style: { backgroundColor: "white", textColor: "blue" },
+    description:
+      "Descripción de la prenda 10: Zapatillas deportivas con amortiguación extra.",
   },
 ];
 
@@ -65,25 +85,35 @@ const Roulette: React.FC = () => {
   };
 
   return (
-    <div className={style.roulette}>
-      <button onClick={handleSpinClick} className={style.buttonSpin}>
-        <Wheel
-          mustStartSpinning={mustSpin}
-          prizeNumber={prizeNumber}
-          data={data}
-          onStopSpinning={() => {
-            setMustSpin(false);
-            setWinningOption(data[prizeNumber].option); // Actualiza la opción ganadora
-          }}
-          outerBorderWidth={2}
-          radiusLineWidth={0}
-          pointerProps={pointerProps}
-        />
-      </button>
-      {winningOption &&
-        !mustSpin && ( // Muestra el texto solo si hay una opción ganadora
-          <div className={style.winningText}>{winningOption}</div>
+    <div className={style.containerRoulette}>
+      <h1 className={style.titleRoulette}>TOQUE PARA GIRAR</h1>
+      <div className={style.roulette}>
+        <button onClick={handleSpinClick} className={style.buttonSpin}>
+          <Wheel
+            mustStartSpinning={mustSpin}
+            prizeNumber={prizeNumber}
+            data={data}
+            onStopSpinning={() => {
+              setMustSpin(false);
+              setWinningOption(data[prizeNumber].option); // Actualiza la opción ganadora
+            }}
+            outerBorderWidth={2}
+            radiusLineWidth={0}
+            pointerProps={pointerProps}
+          />
+        </button>
+        {winningOption && !mustSpin && (
+          <>
+            <div className={style.winningText}>{winningOption}</div>
+            <h2 className={style.subtitleRoulette}>Descripción:</h2>
+            <div className={style.containerText}>
+              <p className={style.textRoulette}>
+                {winningOption && !mustSpin && data[prizeNumber].description}
+              </p>
+            </div>
+          </>
         )}
+      </div>
     </div>
   );
 };
